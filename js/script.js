9c405e3d26cb492daf9e161825afec6a19d3c8e0
IvanTorzoni <torzoni.ivan00@gmail.com>
2024-04-08 15:19:39 +0200

    feat: added script to calculator with comments

---
+++ js/script.js
@@ -0,0 +1,52 @@
+// Cerco lo schermo su HTML
+const inputValue = document.getElementById("user-input");
+
+// Gestione operatori
+// Cerco gli operatori nel file HTML
+const calculate = document.querySelectorAll(".operations").forEach(function (item) {
+    item.addEventListener("click", function (e) {
+
+        // Creo una variabile per l'ultimo valore (se inseriamo nella calcolatrice un numero e un operatore, premendo "=" non darà nessun errore)
+        let lastValue = inputValue.innerText.substring(
+            inputValue.innerText.length,
+            inputValue.innerText.length - 1
+        );
+
+        // Condizioni
+        // Se il valore su schermo è un numero e viene premuto il tasto "="
+        if (!isNaN(lastValue) && e.target.innerHTML === "=") {
+            //Esegui l'operzaione
+            inputValue.innerText = eval(inputValue.innerText);
+            // Altrimenti se premo il pulsante "C"
+        } else if (e.target.innerHTML === "C") {
+            // Resetta il valore a 0
+            inputValue.innerText = 0;
+            // Se il valore è gia a zero
+            if (inputValue.innerText.length == 0) {
+                // quest'ultimo rimarrà zero
+                inputValue.innerText = 0;
+            }
+            // Altrimenti 
+        } else {
+            // se il valore è un numero
+            if (!isNaN(lastValue)) {
+                //inserisci l'operatore premuto
+                inputValue.innerText += e.target.innerHTML;
+            }
+        }
+    });
+});
+
+// Cerco i numeri nel file HTML
+const number = document.querySelectorAll(".num-btn").forEach(function (item) {
+    // Aggiungo l'evento al click ai pulsanti dei numeri
+    item.addEventListener("click", function (e) {
+        // Se il valore su schermo è zero
+        if (inputValue.innerText === "0") {
+            // Sovrascrive con una stringa vuota (senza questo, all' inserimento di un numero comparirà uno zero davanti al primo numero selezionato)
+            inputValue.innerText = "";
+        }
+        // Per aggiungere gli elementi a schermo ed evitare, tramite .trim, possibili spazi
+        inputValue.innerText += e.target.innerHTML.trim();
+    });
+});
